c67f9bf524b9aa21c9541389e67f4678093360cb
Peter <peter@pwang.co>
2016-04-06 18:16:18 -0700

    fix gitignore

--- Example/Pods/Target Support Files/Pods-Segment-CleverTap_Example-Analytics/Pods-Segment-CleverTap_Example-Analytics-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Segment_CleverTap_Example_Analytics : NSObject
-@end
-@implementation PodsDummy_Pods_Segment_CleverTap_Example_Analytics
-@end
--- Example/Pods/Target Support Files/Pods-Segment-CleverTap_Tests-Analytics/Pods-Segment-CleverTap_Tests-Analytics-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Segment_CleverTap_Tests_Analytics : NSObject
-@end
-@implementation PodsDummy_Pods_Segment_CleverTap_Tests_Analytics
-@end
