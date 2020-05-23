6bb8e7471ee8a440254c4618771181b10544961e
Yogesh Singh <yogesh@clevertap.com>
2020-05-23 20:45:22 +0530

    SDK-172: added integration factory init spec

--- SEGCleverTapIntegrationFactorySpec.m
+++ SEGCleverTapIntegrationFactorySpec.m
@@ -1,9 +1,18 @@
 
 @import Quick;
 @import Nimble;
+@import OCMock;
+@import Segment_CleverTap;
 
 QuickSpecBegin(SEGCleverTapIntegrationFactorySpec)
 
-
+describe(@"a Segment CleverTap Integration Factory", ^{
+    
+    it(@"is initialized", ^{
+        
+        SEGCleverTapIntegrationFactory *instance = [SEGCleverTapIntegrationFactory instance];
+        expect(instance).toNot(beNil());
+    });
+});
 
 QuickSpecEnd
