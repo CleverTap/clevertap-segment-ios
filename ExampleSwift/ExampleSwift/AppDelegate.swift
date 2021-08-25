58282c7281e062f224beec40e1be5f57fb5460a9
Aditi3 <Aditi3@users.noreply.github.com>
2021-08-25 16:52:41 +0530

    Update Swift Example Project

--- ExampleSwift/ExampleSwift/AppDelegate.swift
+++ ExampleSwift/ExampleSwift/AppDelegate.swift
@@ -19,6 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Analytics.debug(true)
         CleverTap.setDebugLevel(CleverTapLogLevel.debug.rawValue+1)
         
+        CleverTap.setCredentialsWithAccountID("RKW-R77-KW6Z", andToken: "aa2-032")
+        CleverTap.autoIntegrate()
+        
         let config = AnalyticsConfiguration(writeKey: "qp2acCBE3Ph9v4EhOPpXeJtUXa2xepQz")
         config.use(SEGCleverTapIntegrationFactory())
         Analytics.setup(with: config)
