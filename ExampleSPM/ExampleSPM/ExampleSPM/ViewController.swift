1fc2eee80ece7d5c311794fc9b753f39a1462104
Aditi3 <Aditi3@users.noreply.github.com>
2021-09-30 09:42:26 +0530

    Adds ExampleSPM

---
+++ ExampleSPM/ExampleSPM/ExampleSPM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleSPM
+//
+//  Created by Aditi Agrawal on 30/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
