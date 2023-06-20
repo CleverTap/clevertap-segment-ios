3b1e211186677c9474ec17e37008b7ef95f778f1
Aishwarya Nanna <aishwarya.nanna@clevertap.com>
2023-06-20 13:42:59 +0530

    clevertap-ios-sdk version update in Package.swift file

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(name: "Segment",
                  url: "https://github.com/segmentio/analytics-ios.git", from: "4.1.4"),
         .package(name: "CleverTapSDK",
-                 url: "https://github.com/CleverTap/clevertap-ios-sdk.git", from: "5.0.0"),
+                 url: "https://github.com/CleverTap/clevertap-ios-sdk.git", from: "5.0.1"),
     ],
     targets: [
         .target(
